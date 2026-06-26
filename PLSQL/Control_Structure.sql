3db796431af4f45a6355b6e883cbfa1cc2b8bbc4
Anchal Shukla <152485349+Anchalshukla145@users.noreply.github.com>
2026-06-26 23:26:38 +0530

    Add PL/SQL block to update interest rates for seniors
    This PL/SQL block updates the interest rate for loans of customers older than 60 years.

---
+++ PLSQL/Control_Structure.sql
@@ -0,0 +1,17 @@
+BEGIN
+    FOR rec IN (
+        SELECT l.LoanID
+        FROM Loans l
+        JOIN Customers c
+        ON l.CustomerID = c.CustomerID
+        WHERE FLOOR(MONTHS_BETWEEN(SYSDATE, c.DOB)/12) > 60
+    )
+    LOOP
+        UPDATE Loans
+        SET InterestRate = InterestRate - 1
+        WHERE LoanID = rec.LoanID;
+    END LOOP;
+
+    COMMIT;
+END;
+/
